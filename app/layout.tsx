2ad657efc133de39e193318ec8f6a3c50cb902f9
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2024-03-23 16:39:22 +0100

    customize layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
-import { GeistSans } from "geist/font/sans";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
@@ -11,17 +17,17 @@ export const metadata = {
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased dark:bg-stone-950 text-stone-50 overflow-hidden",
+          fontSans.variable
+        )}
+      >
+        <main className="min-h-screen flex flex-col items-center">{children}</main>
+        <Toaster />
       </body>
     </html>
   );
