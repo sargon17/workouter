5252409be29476aba148d536eb388a0fea30a909
Mykhaylo Tymofyeyev <mykhaylo.tymofyeyev@deseip.com>
2024-03-23 16:40:04 +0100

    Header Layout and logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,19 @@
-import NextLogo from "./NextLogo";
-import SupabaseLogo from "./SupabaseLogo";
+import { createClient } from "@/utils/supabase/server";
+import { ReactNode } from "react";
 
-export default function Header() {
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export default async function Header({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
-      </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    <div className="flex justify-between items-center w-full p-4 border-b-2 dark:border-b-stone-800 dark:text-stone-50 mb-4">
+      <Link
+        href={"/"}
+        className="font-bold text-lg leading-none hover:text-stone-300 transition-colors duration-100 ease-in-out"
+      >
+        <h1 className=" uppercase">Workouter</h1>
+      </Link>
+      <div>{children}</div>
     </div>
   );
 }
