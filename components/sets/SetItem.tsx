5e55ca6419538bcb4587dad558c9f753bb3d3836
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-03-24 16:38:53 +0100

    Refactor SetItem component to include dropdown menu and drawer

--- components/sets/SetItem.tsx
+++ components/sets/SetItem.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+"use client";
 
+import { useState } from "react";
+
+import { Trash, Edit } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+
+import DeleteSetButton from "./DeleteSetButton";
+import EditSetForm from "./EditSetForm";
 export default function SetItem({
   children,
   id,
@@ -11,5 +36,60 @@ export default function SetItem({
   reps: number;
   weight: number;
 }) {
-  return <div>{children}</div>;
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <span>{children}</span>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Set Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={() => {
+              setIsDrawerOpen(true);
+            }}
+          >
+            <Edit className="h-4 w-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+          <DeleteSetButton set_id={id}>
+            <DropdownMenuItem className="text-red-500 dark:focus:bg-red-900 ">
+              <Trash className="h-4 w-4 mr-2" />
+              Delete
+            </DropdownMenuItem>
+          </DeleteSetButton>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Drawer
+        open={isDrawerOpen}
+        onOpenChange={(state) => {
+          setIsDrawerOpen(state);
+        }}
+      >
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Edit Set</DrawerTitle>
+          </DrawerHeader>
+          <EditSetForm
+            set_id={id}
+            original_reps={reps}
+            original_weight={weight}
+          />
+          <DrawerFooter>
+            <DrawerClose
+              onClick={() => {
+                setIsDrawerOpen(false);
+              }}
+            >
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 }
