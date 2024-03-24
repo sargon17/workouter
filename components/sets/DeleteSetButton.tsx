0623a414fc2c814fa396db40fe7dc6d44685512b
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-03-24 16:38:47 +0100

    Add DeleteSetButton component

---
+++ components/sets/DeleteSetButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { createClient } from "@/utils/supabase/client";
+
+import { toast } from "sonner";
+
+import { useRouter } from "next/navigation";
+
+export default function DeleteTargetSetButton({
+  children,
+  set_id,
+}: {
+  children: React.ReactNode;
+  set_id: string;
+}) {
+  const supabase = createClient();
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const { error } = await supabase.from("sets").delete().eq("id", set_id);
+
+    if (error) {
+      toast("Something went wrong while deleting the set");
+    } else {
+      toast("Set deleted");
+      router.refresh();
+    }
+  };
+
+  return <span onClick={handleDelete}>{children}</span>;
+}
