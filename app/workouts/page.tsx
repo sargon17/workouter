cefebb13923b96d0f108a804d1d9c865821da947
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-03-30 21:04:22 +0100

    Refactor searchParams in ProtectedPage function

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -13,7 +13,7 @@ export default async function ProtectedPage(
   { searchParams } = {
     searchParams: {
       t: "upcoming" as "upcoming" | "past",
-    },
+    } as { t: "upcoming" | "past" },
   }
 ) {
   const supabase = createClient();
