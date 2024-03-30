5f34f250d57e9bebe4d6724abc07d1cd3a13d96e
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-03-30 19:24:16 +0100

    No workout layout update

--- components/workout/WorkoutList.tsx
+++ components/workout/WorkoutList.tsx
@@ -34,28 +34,33 @@ export default async function WorkoutList() {
   }
 
   return (
-    <div className="w-full">
-      <div className="mb-4">
-        <h1 className=" text-xl font-bold">Your Next Workouts</h1>
-      </div>
-      <div className="flex flex-wrap gap-2">
-        {workouts.length === 0 && <NoWorkouts />}
-        {workouts.map((workout: any, i: number) => (
-          <WorkoutListItem
-            workout={workout as SingleWorkout}
-            highlighted={i === 0}
-            key={workout.id}
-          />
-        ))}
-      </div>
+    <div className="w-full h-full">
+      {workouts.length > 0 && (
+        <>
+          <div className="mb-4">
+            <h1 className=" text-xl font-bold">Your Next Workouts</h1>
+          </div>
+          <div className="flex flex-wrap gap-2">
+            {workouts.map((workout: any, i: number) => (
+              <WorkoutListItem
+                workout={workout as SingleWorkout}
+                highlighted={i === 0}
+                key={workout.id}
+              />
+            ))}
+          </div>
+        </>
+      )}
+      {workouts.length === 0 && <NoWorkouts />}
     </div>
   );
 }
 
 const NoWorkouts = () => (
   <div className="w-full h-[100%] flex flex-col justify-center items-center">
-    <h1 className="text-2xl font-bold pb-1">No workouts yet </h1>
-    <p className="text-xs text-center pb-4 text-stone-200">
+    <h2 className="text-2xl font-bold">Welcome to your Workouts</h2>
+    <h3 className="text-lg font-medium pb-4">You don't have any workouts yet</h3>
+    <p className="text-xs text-center pb-4 text-stone-300">
       Click the button below to create your first workout
     </p>
     <NewWorkoutButton>
