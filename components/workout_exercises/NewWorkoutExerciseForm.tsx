07b394dafeb1dc72911b656fb81e2b43b7541366
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-03-31 16:41:36 +0200

    optimized exercise pick screen for mobile & set suggestions func init

--- components/workout_exercises/NewWorkoutExerciseForm.tsx
+++ components/workout_exercises/NewWorkoutExerciseForm.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useState, useRef, useEffect, use } from "react";
+
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ChevronsUpDown } from "lucide-react";
+import { ChevronsUpDown, Plus } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -17,6 +19,7 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
+  CommandSeparator,
 } from "@/components/ui/command";
 import {
   Form,
@@ -31,6 +34,8 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { toast } from "sonner";
 import { DrawerClose } from "../ui/drawer";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
+
 import NewExerciseButton from "../exercises/NewExerciseButton";
 
 const FormSchema = z.object({
@@ -38,6 +43,12 @@ const FormSchema = z.object({
 });
 
 export function NewWorkoutExerciseForm({ workout_id, exercises }: { workout_id: string; exercises: any }) {
+  const [isCommandOpen, setIsCommandOpen] = useState(false);
+  const [suggestedSets, setSuggestedSets] = useState([]) as any;
+
+  const closeBtnRef = useRef(null);
+  let user: any;
+
   const supabase = createClient();
   const router = useRouter();
 
@@ -47,6 +58,58 @@ export function NewWorkoutExerciseForm({ workout_id, exercises }: { workout_id: 
       exercise_id: undefined,
     },
   });
+
+  // get current user
+  useEffect(() => {}, []);
+
+  const getUser = async (supabase: any) => {
+    user = await supabase.auth.getUser();
+  };
+
+  useEffect(() => {
+    if (!form.getValues("exercise_id")) return;
+    const exercise_id = form.getValues("exercise_id");
+
+    // get suggested sets for exercise
+    getSuggestedSets(exercise_id);
+
+    // find exercise by id and last workout_exercise by workout_id and exercise_id and get the last set
+
+    // get last set by workout_exercise_id
+  }, [form.getValues("exercise_id")]);
+
+  const getSuggestedSets = async (exercise_id: number) => {
+    const {
+      data: { user },
+    } = (await supabase.auth.getUser()) as any;
+
+    const { data, error } = await supabase
+      .from("workout_exercises")
+      .select("id, workouts(user_id)")
+      .eq("exercise_id", exercise_id)
+      .eq("workouts.user_id", user.id)
+      .limit(1);
+
+    if (error) {
+      console.error("error", error);
+      return;
+    }
+
+    if (!data || data.length === 0) return;
+
+    const { data: sets, error: setsError } = await supabase
+      .from("sets")
+      .select("reps, weight")
+      .order("id", { ascending: true })
+      .eq("workout_exercise_id", data[0].id);
+
+    if (setsError) {
+      console.error("error", setsError);
+      return;
+    }
+
+    setSuggestedSets(sets);
+  };
 
   async function onSubmit() {
     const { data, error } = await supabase
@@ -61,6 +124,12 @@ export function NewWorkoutExerciseForm({ workout_id, exercises }: { workout_id: 
     } else {
       toast("Workout exercise created");
       form.reset();
+
+      if (closeBtnRef.current) {
+        const btn = closeBtnRef.current as HTMLButtonElement;
+        btn.click();
+      }
+
       router.refresh();
     }
   }
@@ -81,51 +150,107 @@ export function NewWorkoutExerciseForm({ workout_id, exercises }: { workout_id: 
           render={({ field }) => (
             <FormItem className="flex flex-col">
               <FormLabel>Exercise</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn("w-[200px] justify-between", !field.value && "text-muted-foreground")}
-                    >
-                      {field.value
-                        ? exercises.find((exercise: any) => exercise.id === field.value)?.title
-                        : "Select a exercise..."}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput placeholder="Search exercise..." />
-                    <CommandList>
-                      <CommandEmpty>
-                        <NewExerciseButton>
-                          <Button>Create New Exercise</Button>
-                        </NewExerciseButton>
-                      </CommandEmpty>
-                      <CommandGroup>
-                        {exercises.map((exercise: any) => (
-                          <div
-                            key={exercise.id}
-                            onClick={() => form.setValue("exercise_id", exercise.id)}
+              {/* <FormControl> */}
+              <Button
+                variant="outline"
+                className={cn("w-full justify-between", !field.value && "text-muted-foreground")}
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setIsCommandOpen(!isCommandOpen);
+                }}
+              >
+                {field.value
+                  ? exercises.find((exercise: any) => exercise.id === field.value)?.title
+                  : "Select a exercise..."}
+                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+              {/* </FormControl> */}
+              <Command
+                className={cn(
+                  "z-10 w-full max-h-[400px] overflow-x-hidden overflow-y-auto bg-stone-950 rounded-lg shadow-md border border-stone-900",
+                  isCommandOpen ? "block" : "hidden"
+                )}
+              >
+                <CommandInput placeholder="Search exercise..." />
+                <CommandList className="overflow-x-hidden">
+                  <CommandEmpty>
+                    <NewExerciseButton>
+                      <Button>Create New Exercise</Button>
+                    </NewExerciseButton>
+                  </CommandEmpty>
+                  <CommandGroup heading="Add new exercise">
+                    <NewExerciseButton>
+                      <CommandItem
+                        className="
+                      flex items-center justify-start bg-stone-950
+                      gap-2 hover:bg-stone-900
+                      "
+                      >
+                        <Plus width={16} />
+                        Create New Exercise
+                      </CommandItem>
+                    </NewExerciseButton>
+                  </CommandGroup>
+                  <CommandSeparator />
+
+                  <CommandGroup
+                    heading="Exercises"
+                    className="overflow-x-hidden overflow-y-auto"
+                  >
+                    <ScrollArea className="h-[200px] w-[full] pointer-events-auto">
+                      {exercises.map((exercise: any) => (
+                        <div
+                          key={exercise.id}
+                          onClick={() => {
+                            form.setValue("exercise_id", exercise.id);
+                            setIsCommandOpen(false);
+                          }}
+                        >
+                          <CommandItem
+                            value={exercise.title}
+                            className={cn(
+                              "flex items-center justify-between bg-stone-950 hover:bg-stone-800",
+                              field.value === exercise.id && "bg-stone-800"
+                            )}
                           >
-                            <CommandItem value={exercise.title}>{exercise.title}</CommandItem>
-                          </div>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
+                            {exercise.title}
+                          </CommandItem>
+                        </div>
+                      ))}
+                    </ScrollArea>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
               <FormMessage />
             </FormItem>
           )}
         />
-        <DrawerClose>
+
+        {suggestedSets.length > 0 && (
+          <div>
+            {suggestedSets.map((set: any) => (
+              <div key={set.id}>
+                <p>
+                  {set.reps} x {set.weight}kg
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
+
+        <div className="flex justify-end gap-2">
+          <DrawerClose>
+            <Button
+              variant="outline"
+              ref={closeBtnRef}
+              type="button"
+            >
+              Cancel
+            </Button>
+          </DrawerClose>
           <Button type="submit">Submit</Button>
-        </DrawerClose>
+        </div>
       </form>
     </Form>
   );
