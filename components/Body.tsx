9240fb0b96007f94bec946d998fba00fe5802f0b
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-03-31 20:52:10 +0200

    Update styles in Body and Header components

--- components/Body.tsx
+++ components/Body.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Body({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <div
-      className="p-4 mt-20 w-full overflow-auto"
+      className="p-4 pt-20 w-full overflow-auto"
       style={{
         height: "calc(100vh - 80px)",
       }}
