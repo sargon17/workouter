af9fdaacc52941dc72ff36af400d1f9eaf1b2bd2
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-04-01 18:01:58 +0200

    created a group directory for all the actual app pages and section

---
+++ app/(app)/workouts/page.tsx
@@ -0,0 +1,41 @@
+import { Plus } from "lucide-react";
+
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import WorkoutList from "@/components/workout/WorkoutList";
+import NewWorkoutButton from "@/components/workout/NewWorkoutButton";
+
+import Header from "@/components/Header";
+import Body from "@/components/Body";
+import { Button } from "@/components/ui/button";
+
+export default async function ProtectedPage({ searchParams }: { searchParams: { t: "upcoming" | "past" } }) {
+  const supabase = createClient();
+  const searchTimeline = searchParams.t;
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  return (
+    <>
+      <Header>
+        <NewWorkoutButton>
+          <Button
+            size="icon"
+            variant="outline"
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+        </NewWorkoutButton>
+      </Header>
+      <Body>
+        <WorkoutList isPast={searchTimeline === "past"} />
+      </Body>
+    </>
+  );
+}
