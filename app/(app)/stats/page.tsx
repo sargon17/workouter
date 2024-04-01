c8baf018121882d293b7bdfb0dc9ccbe9a46e8fe
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-04-01 18:56:43 +0200

    Add Header, Body, and WIPpage components to profile and stats pages

--- app/(app)/stats/page.tsx
+++ app/(app)/stats/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+import Header from "@/components/Header";
+import Body from "@/components/Body";
+import WIPpage from "@/components/WIPpage";
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <Header>
+        <></>
+      </Header>
+      <Body>
+        <WIPpage />
+      </Body>
+    </>
+  );
 }
