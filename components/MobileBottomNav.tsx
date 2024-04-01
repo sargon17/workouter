db3e5dc2ce718aa2dbb09190e97dba7dca3f2916
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-04-01 18:14:37 +0200

    Refactor MobileBottomNav component to include current page indicator

--- components/MobileBottomNav.tsx
+++ components/MobileBottomNav.tsx
@@ -1,23 +1,38 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 
 import { User, TrendingUp, Dumbbell } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 import { Button } from "./ui/button";
 
+import { usePathname } from "next/navigation";
+
 export default function MobileBottomNav() {
+  const pathname = usePathname();
+
   return (
     <div className="fixed bottom-0 left-0 w-full">
-      <div className="border rounded-xl dark:border-stone-900 dark:bg-stone-950/70 backdrop-blur-md dark:text-stone-50 flex p-1 w-[98vw] mx-auto mb-6">
-        <MobileBottomNavItem href={"/workouts"}>
+      <div className="border rounded-xl dark:border-stone-900 dark:bg-stone-950/70 backdrop-blur-md dark:text-stone-50 flex p-1 w-[98vw] mx-auto mb-6 gap-1">
+        <MobileBottomNavItem
+          href={"/workouts"}
+          current={pathname.includes("/workouts")}
+        >
           <Dumbbell className="h-4 w-4" />
           Workouts
         </MobileBottomNavItem>
-        <MobileBottomNavItem href={"/stats"}>
+        <MobileBottomNavItem
+          href={"/stats"}
+          current={pathname.includes("/stats")}
+        >
           <TrendingUp className="h-4 w-4" />
           Stats
         </MobileBottomNavItem>
-        <MobileBottomNavItem href={"/profile"}>
+        <MobileBottomNavItem
+          href={"/profile"}
+          current={pathname.includes("/profile")}
+        >
           <User className="h-4 w-4" />
           Profile
         </MobileBottomNavItem>
@@ -26,14 +41,24 @@ export default function MobileBottomNav() {
   );
 }
 
-const MobileBottomNavItem = ({ href, children }: { href: string; children: React.ReactNode }) => {
+const MobileBottomNavItem = ({
+  href,
+  children,
+  current = false,
+}: {
+  href: string;
+  children: React.ReactNode;
+  current?: boolean;
+}) => {
   return (
     <Link
       href={href}
       className="w-full flex justify-center items-center"
     >
       <Button
-        className="flex gap-2 w-full justify-center items-center"
+        className={cn("flex gap-2 w-full justify-center items-center text-stone-400", {
+          "text-stone-50": current,
+        })}
         variant={"ghost"}
       >
         {children}
