de9c15a36822b6b3ba17c13bc8b1cf043d5f56a1
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-04-03 22:24:17 +0200

    Refactor DatePicker component

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -14,40 +14,38 @@ export function DatePicker({ field }: any) {
 
   return (
     <>
-      {isMobile && (
-        <div>
-          <Button
-            variant="outline"
-            type="button"
-            className={cn(
-              "w-[280px] justify-start text-left font-normal",
-              !field.value && "text-muted-foreground"
-            )}
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-          </Button>
+      <div>
+        <Button
+          variant="outline"
+          type="button"
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !field.value && "text-muted-foreground"
+          )}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+        </Button>
 
-          <div
-            className={cn("", {
-              hidden: !isOpen,
-              block: isOpen,
-            })}
-          >
-            <Calendar
-              mode="single"
-              selected={field.value}
-              onSelect={(e: any) => {
-                field.onChange(e);
-                setIsOpen(false);
-              }}
-              initialFocus
-              weekStartsOn={1}
-            />
-          </div>
+        <div
+          className={cn("", {
+            hidden: !isOpen,
+            block: isOpen,
+          })}
+        >
+          <Calendar
+            mode="single"
+            selected={field.value}
+            onSelect={(e: any) => {
+              field.onChange(e);
+              setIsOpen(false);
+            }}
+            initialFocus
+            weekStartsOn={1}
+          />
         </div>
-      )}
+      </div>
     </>
   );
 }
