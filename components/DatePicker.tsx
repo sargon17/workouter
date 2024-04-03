dced574fa9ad98f5121b4157adaa9009b22f001c
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-04-03 22:51:03 +0200

    Refactor calendar component and add date picker dialog

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -5,47 +5,80 @@ import { useState } from "react";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 
+import { DayPicker } from "react-day-picker";
+
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export function DatePicker({ field }: any) {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
-      <div>
-        <Button
-          variant="outline"
-          type="button"
-          className={cn(
-            "w-[280px] justify-start text-left font-normal",
-            !field.value && "text-muted-foreground"
-          )}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-        </Button>
+      <Dialog>
+        <div>
+          <DialogTrigger asChild>
+            <Button
+              variant="outline"
+              type="button"
+              className={cn(
+                "w-[280px] justify-start text-left font-normal",
+                !field.value && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Select a date</DialogTitle>
+            </DialogHeader>
+            <div className="w-full flex justify-center items-center">
+              <Calendar
+                mode="single"
+                selected={field.value}
+                onSelect={(e: any) => {
+                  field.onChange(e);
+                }}
+                initialFocus
+                weekStartsOn={1}
+              />
+              {/* <DayPicker
+              mode="single"
+              selected={field.value}
+              onSelect={(e: any) => {
+                field.onChange(e);
+              }}
+            /> */}
+            </div>
+          </DialogContent>
 
-        <div
-          className={cn("", {
-            hidden: !isOpen,
-            block: isOpen,
-          })}
-        >
-          <Calendar
-            mode="single"
-            selected={field.value}
-            onSelect={(e: any) => {
-              field.onChange(e);
-              setIsOpen(false);
-            }}
-            initialFocus
-            weekStartsOn={1}
-          />
+          {/* <DialogFooter>
+            <div className="flex w-full justify-end gap-2">
+              <DrawerClose>
+                <Button
+                  variant="outline"
+                  type="button"
+                >
+                  Cancel
+                </Button>
+              </DrawerClose>
+              <DrawerClose>
+                <Button type="submit">Submit</Button>
+              </DrawerClose>
+            </div>
+          </DialogFooter> */}
         </div>
-      </div>
+      </Dialog>
     </>
   );
 }
