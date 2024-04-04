ded4f1875363b9f3f1176729fe85ff9c4712b62e
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-04-04 15:34:20 +0200

    Refactor Loading component to remove unnecessary Body component

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Body from "./Body";
 
 export default function Loading() {
   return (
-    <Body>
-      <div className="w-full h-full flex justify-center items-center">
-        <div className="text-center">
-          <h2 className="text-3xl font-bold uppercase">Workouter</h2>
-          <p className="text-xs text-pretty text-stone-500">Wait a moment while the page loads...</p>
-        </div>
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="text-center">
+        <h2 className="text-3xl font-bold uppercase">Workouter</h2>
+        <p className="text-xs text-pretty text-stone-500">Wait a moment while the page loads...</p>
       </div>
-    </Body>
+    </div>
   );
 }
