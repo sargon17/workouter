1154f7ebadfe4c0422ccd29655433935957743b0
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-04-17 23:46:40 +0200

    Update navigation styling in HeaderSite.tsx

--- components/HeaderSite.tsx
+++ components/HeaderSite.tsx
@@ -16,12 +16,12 @@ export default function HeaderSite(props: Props) {
     <div className="fixed w-full top-0 left-0 z-[10000] border-b border-b-stone-900 bg-stone-950/70 backdrop-blur-md text-stone-50">
       <div className={cn("grid grid-cols-3 gap-1 w-full p-2 max-w-screen-2xl h-14 mx-auto", {})}>
         <div className=" col-span-1 flex justify-start items-center w-full">
-          <nav className="flex gap-4">
+          <nav className="flex gap-6">
             {props.nav.map((item, index) => (
               <Link
                 key={index}
                 href={item.url}
-                className="text-stone-300 hover:text-stone-200 transition-colors duration-100 ease-in-out font-medium"
+                className="text-stone-400 hover:text-stone-100 transition-colors duration-100 ease-in-out font-medium"
               >
                 {item.label}
               </Link>
