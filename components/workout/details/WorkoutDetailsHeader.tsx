eca4bc894fb099cd63809a0bb9dd6d15387d481f
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-05-31 23:39:03 +0200

    refactored Workout Details Header structure

--- components/workout/details/WorkoutDetailsHeader.tsx
+++ components/workout/details/WorkoutDetailsHeader.tsx
@@ -14,7 +14,6 @@ import { getStatusColor } from "../../status/getStatusColor";
 type WorkoutDetailsHeaderProps = {
   workout: Workout;
   status?: StatusType["name"];
-  //   children?: React.ReactNode;
   chip?: React.ReactNode;
 };
 
@@ -25,14 +24,14 @@ const WorkoutDetailsHeader = (props: WorkoutDetailsHeaderProps) => {
   return (
     <div
       className={cn(
-        " w-full min-h-36 p-2 py-16 my-2 border rounded-2xl flex flex-col justify-center items-center gap-2 relative" +
+        "w-full min-h-56 p-2 py-16 my-2 border rounded-2xl flex flex-col justify-center items-center gap-2 relative" +
           ` border-${color}-500/70 text-${color}-200`
       )}
     >
       <Pattern />
       {props.chip && <div className="absolute top-1 left-1">{props.chip}</div>}
 
-      <div className=" absolute z-10 top-1 right-1">
+      {/* <div className=" absolute z-10 top-1 right-1">
         <SingleWorkoutMoreButton
           id={props.workout.id}
           title={props.workout.title}
@@ -45,31 +44,51 @@ const WorkoutDetailsHeader = (props: WorkoutDetailsHeaderProps) => {
             <MoreHorizontal className="h-4 w-4" />
           </Button>
         </SingleWorkoutMoreButton>
-      </div>
+      </div> */}
       <div className="relative z-10 flex flex-col items-center gap-2">
         <h2 className=" text-balance font-bold text-2xl antialiased capitalize text-center">
           {props.workout.title}
         </h2>
-        <Link href={`/session/${props.workout.id}`}>
-          <Button variant="outline">Start Workout</Button>
-        </Link>
+        {props.workout.workout_statuses?.name === "planed" && <StartWorkoutButton workout={props.workout} />}
       </div>
       <div className=" absolute min-h-12 bottom-0 left-0 w-full ">
-        <div
-          className={cn("absolute inset-1 rounded-lg border p-2 font-bold flex gap-2", {
-            "bg-purple-950 border-purple-800": props.workout.workout_statuses.name === "done",
-            "bg-lime-950 border-lime-800": props.workout.workout_statuses.name === "planed",
-          })}
-        >
-          <p>Exercises: {props.workout.workout_exercises.length}</p>
-          <p>
-            Sets:{" "}
-            {props.workout.workout_exercises.reduce(
-              (acc: any, curr: any) => acc + curr.target_sets.length,
-              0
-            )}
-          </p>
-        </div>
+        <WorkoutDetailsHeaderFooter
+          workout={props.workout}
+          color={color}
+        />
+      </div>
+    </div>
+  );
+};
+
+const StartWorkoutButton = ({ workout }: { workout: Workout }) => {
+  return (
+    <Link href={`/session/${workout.id}`}>
+      <Button variant="outline">Start Workout</Button>
+    </Link>
+  );
+};
+
+type WorkoutDetailsHeaderFooterProps = {
+  workout: Workout;
+  status?: StatusType["name"];
+  color: string;
+};
+const WorkoutDetailsHeaderFooter = (props: WorkoutDetailsHeaderFooterProps) => {
+  const clss = cn(
+    "backdrop-blur-sm border rounded-xl p-2 px-4 w-full" +
+      ` bg-${props.color}-500/5 border-${props.color}-500/70 text-${props.color}-200`
+  );
+
+  return (
+    <div className={cn("absolute inset-1 font-bold flex gap-1")}>
+      <div className={clss}>
+        <span className="text-xs font-light opacity-60">Total sets:</span>{" "}
+        {props.workout.workout_exercises.reduce((acc: any, curr: any) => acc + curr.target_sets.length, 0)}
+      </div>
+      <div className={clss}>
+        <span className="text-xs font-light opacity-60">Total exercises:</span>{" "}
+        {props.workout.workout_exercises.length}
       </div>
     </div>
   );
