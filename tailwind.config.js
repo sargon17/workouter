4a3bfa67b241b351bdfa42525945be777ce996e9
Sargon17 <mykhaylo.tymofyeyev@gmail.com>
2024-06-01 00:06:05 +0200

    updated Timer animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,12 +23,10 @@ module.exports = withUt({
       keyframes: {
         "timer-danger": {
           "0%, 100%": {
-            backgroundColor: "rgb(244 63 94 / 0.1)", // red
             borderColor: "rgb(251 113 133 / 0.5)", // red
             color: "rgb(251 113 133 / var(--tw-text-opacity))", // red
           },
           "50%": {
-            backgroundColor: "rgb(132 204 22 / 0.1)", // green
             borderColor: "rgb(190 242 100 / 0.5)", // green
             color: "rgb(190 242 100 / var(--tw-text-opacity))", // green
           },
